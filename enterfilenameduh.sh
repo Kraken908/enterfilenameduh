65776d4d37798076aa58896c434c463d30f08b0d
kraken <70107358+Kraken908@users.noreply.github.com>
2023-01-23 21:04:05 -0700

    Update enterfilenameduh.sh

--- enterfilenameduh.sh
+++ enterfilenameduh.sh
@@ -6,4 +6,4 @@ echo "Document directory usage report" > /tmp/report
 du -sh $dir >> /tmp/report
 echo "Start of report: $start" >> /tmp/report
 echo "End of report: $(date)" >> /tmp/report
-# Concatenate the file in /tmp/report after running the script
+# Concatenate the file  /tmp/report after running the script
